84697360ff3426e7f480527bd0bdbe02620a6597
Dennis Keefe <dkeefe75@gmail.com>
2013-03-29 23:21:52 -0500

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyfriends::Application.config.secret_token = '99db15ea30d0aad4c8396b1f58993d419be378afde61799d501c0bcb7f0f7a1ae4a845872beee8b2f18c58fb6c51486aaf8d54e58bd626865787874c4857f815'
